7488e5aae618ef6f71fba3da58ffdbfb6a1e9df8
Feiyang Chen <chenfeiyang@loongson.cn>
2022-03-21 16:19:30 +0800

    loongarch64: Add support for emitting event check instruction

--- VEX/priv/host_loongarch64_defs.h
+++ VEX/priv/host_loongarch64_defs.h
@@ -408,10 +408,11 @@ typedef enum {
       condition (which could be LAcc_AL). */
    LAin_Call,       /* call */
 
-   /* The following 3 insns are mandated by translation chaining */
+   /* The following 4 insns are mandated by translation chaining */
    LAin_XDirect,    /* direct transfer to GA */
    LAin_XIndir,     /* indirect transfer to GA */
-   LAin_XAssisted   /* assisted transfer to GA */
+   LAin_XAssisted,  /* assisted transfer to GA */
+   LAin_EvCheck     /* Event check */
 } LOONGARCH64InstrTag;
 
 typedef struct {
@@ -527,6 +528,10 @@ typedef struct {
          LOONGARCH64CondCode  cond;
          IRJumpKind           jk;
       } XAssisted;
+      struct {
+         LOONGARCH64AMode*    amCounter;
+         LOONGARCH64AMode*    amFailAddr;
+      } EvCheck;
    } LAin;
 } LOONGARCH64Instr;
 
@@ -588,6 +593,8 @@ extern LOONGARCH64Instr* LOONGARCH64Instr_XAssisted ( HReg dstGA,
                                                       LOONGARCH64AMode* amPC,
                                                       LOONGARCH64CondCode cond,
                                                       IRJumpKind jk );
+extern LOONGARCH64Instr* LOONGARCH64Instr_EvCheck   ( LOONGARCH64AMode* amCounter,
+                                                      LOONGARCH64AMode* amFailAddr );
 
 extern void ppLOONGARCH64Instr ( const LOONGARCH64Instr* i, Bool mode64 );
 
